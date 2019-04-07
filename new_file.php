fa2191cb5909f53c8935ef93f2d4e9e1d22a0c8d
xuexiangyang <1109833325@qq.com>
2019-04-07 20:49:49 +0800

    contact 页面完成

--- new_file.php
+++ new_file.php
@@ -1,21 +1,30 @@
+<?php
+	$title=array("首页","社区精选","关于我们","加入我们","招商合作","品牌账号",);
+?>	
+	
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Contact</title>
-	<link rel="stylesheet" href="./css/base.css">
-	<link rel="stylesheet" href="./css/same.css">
-	<link rel="stylesheet" type="text/css" href="css/contact.css"/>
-	<script src="./js/jquery-1.10.1.js"></script>
-	<script src="./js/underscore.js"></script>
-</head>
-<body>
-	<header></header>
-	<section>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+        <link rel="stylesheet" type="text/css" href="css/new_file.css"/>
+	</head>
+	<body>
+		<div id="zt">
+			<div id="head">
+				<img src="img/微信图片_20190407185941.png"/>
+			<ul>
+				<li>首页</li>
+				<li>社区精选</li>
+				<li>关于我们</li>
+				<li></li>
+				<li></li>
+				<li></li>
+	
+			</ul>
+		</div>
 		<div id="pic">
-	          <img src="img/contact/微信图片_20190407185837.png"/> 
+	          <img src="img/微信图片_20190407185837.png"/> 
 		</div>
 		<div id="body1">
 			<h6 class="b1">COOPERATIONS</h6><p class="b2">合作邮箱</p>
@@ -43,19 +52,26 @@
 				<div><h5>武汉</h5><p>武汉市 光谷大道 现代光谷世贸中心B座10楼</p></div>
 			</div>
 			<div class="pot">
-				<img src="img/contact/2.jpg"/>
+				<img src="img/2.jpg"/>
 			</div>
 		</div>
 	    <div id="body3">
 	    	<h3 class="d1">MORE</h3><p class="d2">更多</p>
-	    	<div><img src="img/contact/QQ图片20190407201738.png"/></div>
-	    	<div><img src="img/contact/QQ图片20190407201750.png"/></div>
-	    	<div><img src="img/contact/QQ图片20190407201754.png"/></div>
-	    	<div><img src="img/contact/QQ图片20190407201756.png"/></div>
-	    	<div><img src="img/contact/QQ图片20190407201802.png"/></div>
+	    	<div><img src="img/QQ图片20190407201738.png"/></div>
+	    	<div><img src="img/QQ图片20190407201750.png"/></div>
+	    	<div><img src="img/QQ图片20190407201754.png"/></div>
+	    	<div><img src="img/QQ图片20190407201756.png"/></div>
+	    	<div><img src="img/QQ图片20190407201802.png"/></div>
 	    	
 	    </div>
-	</section>
-	<footer></footer>
-</body>
+		<div id="bottom1">
+			<div><img src="img/4.jpg"/></div>
+			<div><h4>公司相关</h4><p></p><p></p><p></p></div>
+				
+			
+		</div>
+		</div>
+	</body>
 </html>
+<script></script>
+
